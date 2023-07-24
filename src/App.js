8b9b81073f869efed7145c32ecae534919e270d8
jagrecco <96453177+jagrecco@users.noreply.github.com>
2023-07-24 11:41:58 -0300

    First commit on Tailwind & React exploration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+/* import logo from './logo.svg'; */
+/* import './App.css'; */
 
 function App() {
   return (
     <div className="App">
+      <h1 className="text-fuchsia-700">Hola</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
